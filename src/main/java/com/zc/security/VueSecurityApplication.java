9c5a907a16781dec3841e90b24f56cf075234fe4
zhouchi-git <2072416702@qq.com>
2021-07-30 15:20:00 +0800

    修改Application.java
    Signed-off-by: zhouchi-git <2072416702@qq.com>

--- src/main/java/com/zc/security/VueSecurityApplication.java
+++ src/main/java/com/zc/security/VueSecurityApplication.java
@@ -1,9 +1,11 @@
 package com.zc.security;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.zc.security.mapper")
 public class VueSecurityApplication {
 
     public static void main(String[] args) {
