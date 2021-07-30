223534833273e0d2bdd53a1cab1f954742edcc0c
zhouchi-git <2072416702@qq.com>
2021-07-30 14:17:23 +0800

    1.0V版本
    Signed-off-by: zhouchi-git <2072416702@qq.com>

---
+++ src/main/java/com/zc/security/VueSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.zc.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zc/security/VueSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zc.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
